2784024be92b66a7aa7c5af373d48c3f609a8280
ellamurii <emb.baguio@gmail.com>
2021-09-10 20:28:10 +0800

    build fix

--- components/layouts/Main/index.tsx
+++ components/layouts/Main/index.tsx
@@ -23,7 +23,7 @@ const Main = ({ children, title = "Ellamurii" }: Props) => (
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
         href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
         rel="stylesheet"
